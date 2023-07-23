e0d30eef47afc9a15ca68b748500dc41056b7bdf
hoosnick <hoosnick000@gmail.com>
2023-07-23 14:11:18 +0500

    new file:   qrcodebot/buttons/keyboards.py

---
+++ qrcodebot/buttons/keyboards.py
@@ -0,0 +1,58 @@
+from telebot import types
+
+
+class Buttons:
+    def __init__(self):
+        self.IKM = types.InlineKeyboardMarkup
+        self.IKB = types.InlineKeyboardButton
+        self.IQRCP = types.InlineQueryResultCachedPhoto
+
+    def main_menu(self) -> types.InlineKeyboardMarkup:
+        return self.IKM(
+            keyboard=[
+                [self.IKB('*️⃣ Inline QR',
+                          switch_inline_query_current_chat='hello')],
+                [
+                    self.IKB('⚙️ Styles', callback_data='change_style'),
+                    self.IKB('About ℹ️', callback_data='about')
+                ]
+            ]
+        )
+
+    def faq(self) -> types.InlineKeyboardMarkup:
+        return self.IKM(
+            [
+                [
+                    self.back(),
+                    self.IKB('Developer', 'https://t.me/hoosnick')
+                ],
+            ]
+        )
+
+    def styles(self) -> types.InlineKeyboardMarkup:
+        return self.IKM(
+            [
+                [
+                    self.IKB('Square', callback_data='square_style'),
+                    self.IKB('Gapped Square',
+                             callback_data='gappedsquare_style'),
+                ],
+                [
+                    self.IKB('Circle', callback_data='circle_style'),
+                    self.IKB('Rounded', callback_data='rounded_style'),
+                ],
+                [
+                    self.IKB('Vertical Bars',
+                             callback_data='verticalbars_style'),
+                    self.IKB('Horizontal Bars',
+                             callback_data='horizontalbars_style'),
+                ],
+                [self.back()]
+            ]
+        )
+
+    def back(self) -> types.InlineKeyboardButton:
+        return self.IKB('◀️ Back', callback_data='back')
+
+    def inlinePhoto(self, id: int, img: dict) -> types.InlineQueryResultCachedPhoto:
+        return self.IQRCP(id=id, title="qr_code", photo_file_id=img["photo_file_id"])
