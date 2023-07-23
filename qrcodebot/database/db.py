23263e3e44a58189fd31bb526f8e547c90b9eb53
hoosnick <hoosnick000@gmail.com>
2023-07-23 14:12:32 +0500

    optimized and added pydantic v2 (test)

--- qrcodebot/database/db.py
+++ qrcodebot/database/db.py
@@ -1,63 +1,75 @@
-import sqlite3
+from contextlib import closing
+from sqlite3 import connect
+from loguru import logger
+
+import pydantic
+
+
+class User(pydantic.BaseModel):
+    id: int
+    name: str
+    style: int
 
 
 class DataBase:
     def __init__(self):
-        self.conn = sqlite3.connect('data.db', check_same_thread=False)
-        self.cur = self.conn.cursor()
+        self.initialize()
 
-    def create(self):
-        with self.conn:
-            is_table_exists = self.cur.execute(
-                "SELECT name FROM sqlite_master WHERE type='table' AND name='Users'",
-            ).fetchone()
-            if not is_table_exists:
-                query = """CREATE TABLE IF NOT EXISTS Users(
+    def initialize(self):
+        with closing(connect("data.db")) as conn:
+            with closing(conn.cursor()) as cursor:
+                query = """
+                CREATE TABLE IF NOT EXISTS Users(
                     user_id INT,
                     user_name TEXT,
-                    user_design INTEGER DEFAULT 1)"""
-                self.cur.execute(query)
+                    user_design INTEGER DEFAULT 1
+                )"""
+                cursor.execute(query)
+                conn.commit()
 
     def new_user(self, user_id: int, user_name: str):
-        with self.conn:
-            n = self.cur.execute(
-                'SELECT * FROM `Users` WHERE `user_id` = ?', (user_id,)
-            ).fetchall()
-            if bool(len(n)):
-                return
-            query = "INSERT INTO `Users` (`user_id`, `user_name`) VALUES(?,?)"
-            return self.cur.execute(query, (user_id, user_name))
+        with closing(connect("data.db")) as conn:
+            with closing(conn.cursor()) as cursor:
+                q = 'SELECT * FROM `Users` WHERE `user_id` = ?'
+                n = cursor.execute(q, (user_id,)).fetchall()
+
+                if bool(len(n)):
+                    return
+
+                query = "INSERT INTO Users(user_id, user_name) VALUES(?,?)"
+                cursor.execute(query, (user_id, user_name,))
+                conn.commit()
 
     def update_name(self, user_id: int, new_name: str):
-        with self.conn:
-            user = self.get_user(user_id)
-            if user.name != new_name:
-                query = "UPDATE `Users` SET `user_name` = (?) WHERE `user_id` = (?)"
-                return self.cur.execute(query, (new_name, user_id))
+        with closing(connect("data.db")) as conn:
+            with closing(conn.cursor()) as cursor:
+                user = self.get_user(user_id)
+                if user.name == new_name:
+                    return
+
+                q = "UPDATE `Users` SET `user_name` = (?) WHERE `user_id` = (?)"
+                cursor.execute(q, (new_name, user_id,))
+                conn.commit()
 
     def update_style(self, user_id: int, new_style: int):
-        with self.conn:
-            user = self.get_user(user_id)
-            if user.style != new_style:
-                query = "UPDATE `Users` SET `user_design` = (?) WHERE `user_id` = (?)"
-                return self.cur.execute(query, (new_style, user_id))
+        with closing(connect("data.db")) as conn:
+            with closing(conn.cursor()) as cursor:
+                user = self.get_user(user_id)
+                if user.style == new_style:
+                    return
 
-    def get_user(self, user_id: int):
-        with self.conn:
-            result = self.cur.execute(
-                'SELECT * FROM `Users` WHERE `user_id` = ?', (user_id,)
-            ).fetchone()
+                q = "UPDATE `Users` SET `user_design` = (?) WHERE `user_id` = (?)"
+                cursor.execute(q, (new_style, user_id,))
+                conn.commit()
 
-            return dotDict({
-                'id': result[0],
-                'name': result[1],
-                'style': result[2],
-            })
+    def get_user(self, user_id: int) -> User:
+        with closing(connect("data.db")) as conn:
+            with closing(conn.cursor()) as cursor:
+                q = 'SELECT * FROM `Users` WHERE `user_id` = ?'
+                result = cursor.execute(q, (user_id,)).fetchone()
 
-
-class dotDict:
-    def __init__(self, new_dict: dict):
-        self.dict = new_dict
-
-    def __getattr__(self, val):
-        return self.dict[val]
+                return User(
+                    id=result[0],
+                    name=result[1],
+                    style=result[2]
+                )
